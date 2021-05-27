3fa3245fcdb60cfc74979f9167c73c506446fad4
MatijaMi <matijami12@gmail.com>
2021-05-27 18:26:54 +0200

    Removed canvas for now, pure JS implementation

--- mainScript.js
+++ mainScript.js
@@ -3,187 +3,155 @@ var room = -1;
 var prevWall =0;
 var zoomedIn =false;
 
-function setupCanvas() {
-    let dpi = window.devicePixelRatio;
-    var canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
-    let style_width = +getComputedStyle(canvas).getPropertyValue("width").slice(0, -2);
-    let style_height = +getComputedStyle(canvas).getPropertyValue("height").slice(0, -2);
-    canvas.setAttribute('height', style_height * dpi);
-    canvas.setAttribute('width', style_width * dpi);
-    canvas.addEventListener('click', determineClick, false);
-    drawStartScreen(ctx);
+function startGame(){
+   if(document.getElementById("startButton")!=null){ document.getElementById("startButton").parentElement.removeChild(document.getElementById("startButton"));
+    document.getElementById("startText").parentElement.removeChild(document.getElementById("startText"));
+    }
+    if(document.getElementById("endButton")!=null){
+        document.getElementById("endButton").parentElement.removeChild(document.getElementById("endButton"));
+        document.getElementById("gameOverText").parentElement.removeChild(document.getElementById("gameOverText"));
+    }
+    wall=0;
+    room=0;
+    drawRoom();
+    startCountdown();
 }
-
-function drawStartScreen(ctx){
-    ctx.font = '64px serif';
-    ctx.fillText("Welcome to the Lab", 670, 250);
-    
-    ctx.fillRect(880,400,100,60);
-    ctx.font = '44px serif';
-    ctx.fillStyle= 'rgb(0,0,255)';
-    ctx.fillText("Start", 887, 445);
-    
-}
-
 function drawRoom(){
-    var canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
     var img = new Image();
-    
     switch (wall){
         case 0:
-            img.src = 'NorthWall.png';
+            img.src = 'Images/NorthWall.png';
             break;
         case 1:
-            img.src = 'EastWall.png';
+            img.src = 'Images/EastWall.png';
             break;
         case 2:
-            img.src = 'SouthWall.png';
+            img.src = 'Images/SouthWall.png';
             break;
         case 3:
-            img.src = 'WestWall.png';
+            img.src = 'Images/WestWall.png';
             break;
         case 4:
-            img.src = 'Ceiling.png';
+            img.src = 'Images/Ceiling.png';
             break;
     }
     
     img.onload = function() {
-        ctx.drawImage(img, 0, 0);
-        drawUI(ctx);
+        document.getElementById("backgroundImg").src=img.src;
+        document.getElementById("backgroundImg").style.width="100vw"
+        document.getElementById("backgroundImg").style.height="100vh";
+        document.getElementById("mainDiv").style.backgroundColor="#ffff";
+        document.getElementById("Screwdriver").src="Images/Screwdriver.svg";
+        document.getElementById("Screwdriver").addEventListener("click",pickupScrewdriver);
+        drawUI();
     };
+    
 }
 
-function drawUI(ctx){
-    drawInventory(ctx);
-    
-    ctx.fillStyle="rgb(0,0,0)";
-    
-    if(wall<4){
-        drawLeftArrow(ctx);
-        drawRightArrow(ctx);
-        drawTopArrow(ctx);
-        
+function pickupScrewdriver(){
+    document.getElementById("Screwdriver").style.top="90%";
+    document.getElementById("Screwdriver").style.left="5%";
+}
+
+function drawUI(){
+    drawInventory();    
+    showArrow("left",wall<4);
+    showArrow("right",wall<4);
+    showArrow("top",wall<4);
+    showArrow("bottom", !(wall<4));
+     document.getElementById("topArrow").addEventListener("click",moveTop);
+    document.getElementById("bottomArrow").addEventListener("click",moveBottom);
+    document.getElementById("leftArrow").addEventListener("click",moveLeft);
+    document.getElementById("rightArrow").addEventListener("click",moveRight);
+}
+
+function showArrow(arrow,show){
+    if(show){
+        document.getElementById(arrow+"Arrow").style.opacity="100%";
     }else{
-        drawBottomArrow(ctx);
+        document.getElementById(arrow+"Arrow").style.opacity="0%";
     }
 }
 
-function determineClick(event){
-    var x = event.pageX;
-    var y = event.pageY;
-    console.log(y);
-    console.log(x);
-    if(inBounds(x,y,880,980,400,460) && room<0){
-        room =0;
-        wall =0;
-        drawRoom();
-    }
-    
-    if(inBounds(x,y,10,30,400,500)){
-        if(wall==0){
-            wall=3;
-        }else{
-            wall-=1;
-        }
-        drawRoom();
-    }
-    
-    if(inBounds(x,y,1890,1910,400,500)){
-        wall = (wall +1)%4;
-        drawRoom();
-    }
-    
-    if(inBounds(x,y,880,980,10,30)){
-        prevWall=wall;
-        wall=4;
-        drawRoom();
-    }
-    
-    if(inBounds(x,y,880,980,930, 950) && wall==4){
-        wall = prevWall;
-        drawRoom();
-        
-    }
-    if(inBounds(x,y,880,980,930, 950) && zoomedIn==true){
-        drawRoom();
-        zoomedIn=false;
-        
-    }
-    if(wall==0 && zoomedIn==false && inBounds(x,y,1490,1660,225,385)){
-        drawClock();
-    }
-        
+function drawInventory(){
+   document.getElementById("inventory").style.opacity="100%";
 }
-
-function inBounds(x,y, x1,x2,y1,y2){
-    return x>=x1 && x<=x2 && y>=y1 && y<=y2;
+function moveTop(){
+    prevWall=wall;
+    wall=4;
+    drawRoom();
 }
-    
-
-function drawInventory(ctx){
-    var width =600;
-    var height =80;
-    ctx.strokeStyle= "rgb(0,0,0)";
-    ctx.lineWidth = 6;
-    ctx.strokeRect(20,860,width,height);
-    ctx.fillStyle="rgb(120,120,10)";
-    ctx.fillRect(20,860,width, height);
-    
-    ctx.fillStyle="rgb(255,120,40)";
-    for(var i =0; i <9; i++){
-        ctx.fillRect(28 + 65*i, 870, 60, 60);
-        
+function moveLeft(){
+    console.log("left")
+    if(wall==0){
+        wall=3;
+    }else{
+        wall-=1;
     }
+    drawRoom();
 }
-
-function drawLeftArrow(ctx){
-    ctx.beginPath();
-    ctx.moveTo(30,400);
-    ctx.lineTo(30,500);
-    ctx.lineTo(10,450);
-    ctx.fill();
+function moveRight(){
+    console.log("right")
+    wall=(wall+1)%4;
+    drawRoom();
 }
-
-function drawRightArrow(ctx){
-    ctx.beginPath();
-    ctx.moveTo(1890,400);
-    ctx.lineTo(1890,500);
-    ctx.lineTo(1910,450);
-    ctx.fill(); 
-}
-
-function drawTopArrow(ctx){
-    ctx.beginPath();
-    ctx.moveTo(880,30);
-    ctx.lineTo(980,30);
-    ctx.lineTo(930,10);
-    ctx.fill();
-}
-
-function drawBottomArrow(ctx){
-    ctx.fillStyle="rgb(0,0,0)";
-    ctx.beginPath();
-    ctx.moveTo(880,930);
-    ctx.lineTo(980,930);
-    ctx.lineTo(930,950);
-    ctx.fill();
+function moveBottom(){
+    wall=prevWall;
+    drawRoom();
 }
 
 function drawClock(){
-    var canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
     var img = new Image();
     img.src="Clock.png";
     img.onload = function() {
-        ctx.drawImage(img, 600, 100); 
+       document.getElementById("mainDiv").style.backgroundImage="url("+ img.src +")";
     };
     zoomedIn=true;
-    drawInventory(ctx);
-    drawBottomArrow(ctx);
+    drawInventory();
+    drawBottomArrow();
+}
+function addListeners(){
+    document.getElementById("startButton").addEventListener("click",startGame);
+}
+function gameOver(){
+    document.getElementById("mainDiv").innerHTML+="<p id='gameOverText'>GAME OVER</p><br><button id='endButton'>Restart</button>";
+    document.getElementById("mainDiv").style.backgroundColor="#000";
+    document.getElementById("backgroundImg").src="";
+    document.getElementById("backgroundImg").style.width=0;
+    document.getElementById("backgroundImg").style.height=0;
+    document.getElementById("Screwdriver").src="";
+    document.getElementById("endButton").addEventListener("click",startGame);
+}
+function startCountdown(){
+    document.getElementById("countdownClock").style.opacity="100%";
+    document.getElementById("countdownClock").innerHTML="30:00";
+    
+    function waitaSec(){
+        setTimeout(updateCountdown,1000)
+    }   
+    function updateCountdown(){
+        var time =document.getElementById("countdownClock").innerHTML;
+        var minutes =parseInt(time.substr(0,2));
+        var seconds = parseInt(time.substr(3,4));
+        if(seconds==0){
+            if(minutes==0){
+                gameOver();
+                return;
+            }else{
+              minutes-=1;
+                seconds=59;  
+            }
+        }else{
+            seconds-=1;
+        }
+        if(seconds<10){
+            seconds="0"+seconds;
+        }
+        if(minutes<10){
+            minutes="0"+minutes;
+        }
+        document.getElementById("countdownClock").innerHTML=minutes+":"+seconds;
+        waitaSec();
+    }
+    setTimeout(updateCountdown,1000);
 }
